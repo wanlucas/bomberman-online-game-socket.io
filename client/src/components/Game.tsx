f9b165a81cdea095279155a4b0d466a497dac3b3
Wan Lucas <wan.lucas.90@gmail.com>
2023-02-04 22:32:14 -0300

    feat: initial Game structure

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -1,21 +1,18 @@
 import styled from 'styled-components';
-import io from 'socket.io-client'
 import { useEffect } from 'react';
-import Match from '../game/Match';
+import OnlineGame from '../game/OnlineGame';
 
 const Canvas = styled.canvas`
   border: 1px solid white;
 `;
 
 export default function() {
-  const socket = io('localhost:3001');
-
   useEffect(() => {
-      const canvas = document.getElementById('game') as HTMLCanvasElement;
-      const context = canvas.getContext('2d') as CanvasRenderingContext2D;
-      const match = new Match(context);
+    const canvas = document.getElementById('game') as HTMLCanvasElement;
+    const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+    const game = new OnlineGame(context, { width: canvas.width, height: canvas.height });
       
-      match.start();
+    game.run();
   }, []);
 
   return (
